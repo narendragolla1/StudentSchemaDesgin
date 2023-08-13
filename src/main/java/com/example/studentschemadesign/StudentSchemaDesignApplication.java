9b5938f49d6e678bbab91b474b3caa4e9f702443
narendragolla <narendragolla44@gmail.com>
2023-08-13 14:34:24 +0530

    Final Commit

---
+++ src/main/java/com/example/studentschemadesign/StudentSchemaDesignApplication.java
@@ -0,0 +1,13 @@
+package com.example.studentschemadesgin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentSchemaDesignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentSchemaDesignApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/studentschemadesign/models/Exam.java
@@ -0,0 +1,2 @@
+package com.example.studentschemadesign.models;public class Exam {
+}
